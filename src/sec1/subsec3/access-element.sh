9a07111886dc552f6b755d3daaf57a2e8da24c91
fjktkm <fjktkm@gmail.com>
2024-05-08 11:22:20 +0000

    feat: Add Sec. 1.3

---
+++ src/sec1/subsec3/access-element.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ghci <<EOF
+"Steve Buscemi" !! 6
+[9.4,33.2,96.2,11.2,23.25] !! 1
+EOF
---
+++ src/sec1/subsec3/list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ghci <<EOF
+let lostNumbers = [4,8,15,16,23,42]
+lostNumbers
+EOF

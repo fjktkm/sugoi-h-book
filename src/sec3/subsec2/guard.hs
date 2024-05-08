fab57d4132e98652cdfb83eb2e93a282e91ac4b9
fjktkm <fjktkm@gmail.com>
2024-05-08 16:48:28 +0000

    fix: スペルミスを修正

--- src/sec3/subsec2/guard.hs
+++ src/sec3/subsec2/guard.hs
@@ -3,7 +3,7 @@ bmiTell weight height
   | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
   | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
   | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
-  | otherwise = "You're a whole, congratulations!"
+  | otherwise = "You're a whale, congratulations!"
 
 max' :: (Ord a) => a -> a -> a
 max' a b

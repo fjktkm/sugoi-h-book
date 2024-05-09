8ae8cb0726217a710ea28723e64d71291f5396aa
fjktkm <fjktkm@gmail.com>
2024-05-09 15:27:26 +0000

    feat: Add Sec. 5.5

---
+++ src/sec5/subsec5/fold.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ghci <<EOF
+:l src/sec5/subsec5/fold
+:t foldl
+sum' [3, 5, 2, 1]
+:t foldl
+:t foldr
+and' (repeat False)
+scanl (+) 0 [3, 5, 2, 1]
+scanr (+) 0 [3, 5, 2, 1]
+scanl1 (\acc x -> if x > acc then x else acc) [3, 4, 5, 3, 7, 9, 2, 1]
+scanl (flip (:)) [] [3, 2, 1]
+sqrtSums
+sum (map sqrt [1 .. 131])
+sum (map sqrt [1 .. 130])
+EOF

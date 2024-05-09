707fb43806d571ca7b0b7eaaf869f500b017aac9
fjktkm <fjktkm@gmail.com>
2024-05-09 15:44:31 +0000

    refactor: 無駄なスペースを除去

--- src/sec5/subsec6/apply-operator.sh
+++ src/sec5/subsec6/apply-operator.sh
@@ -3,6 +3,6 @@
 ghci <<EOF
 sum (filter (> 10) (map (*2) [2 .. 10]))
 sum $ filter (> 10) (map (*2) [2 .. 10])
-sum $ filter (> 10)  $ map (*2) [2 .. 10]
+sum $ filter (> 10) $ map (*2) [2 .. 10]
 map ($ 3) [(4 +), (10 *), (^ 2), sqrt]
 EOF

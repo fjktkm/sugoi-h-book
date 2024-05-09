e7b97592d07c7300bf461b4ab8d547e95da99842
fjktkm <fjktkm@gmail.com>
2024-05-09 17:45:14 +0000

    feat: Add Sec. 6.1

---
+++ src/sec6/subsec1/module.hs
@@ -0,0 +1,8 @@
+-- import Data.List
+import Data.List (nub, sort)
+-- import Data.List hiding (nub)
+-- import qualified Data.Map
+import qualified Data.Map as M
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
---
+++ src/sec6/subsec1/module.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ghci <<EOF
+:m + Data.List
+:m + Data.List Data.Map Data.Set
+EOF

dfda2ec4e7b8f5df36789ae678faf76caee0b30b
fjktkm <fjktkm@gmail.com>
2024-05-09 06:59:54 +0000

    feat: Update Sec. 5.1

--- src/sec5/subsec1/currying.sh
+++ src/sec5/subsec1/currying.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+cd src/sec5/subsec1
 ghci <<EOF
+:l currying
 max 4 5
 (max 4) 5
 :t max
 let multTwoWithNine = multThree 9
 multTwoWithNine 2 3
 compareWithHundred 99
+divideByTen 200
+200 / 10
+(/10) 200
+multThree 3 4
 EOF

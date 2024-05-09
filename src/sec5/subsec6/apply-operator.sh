38f8a89b7b8253a43924ac026b65556ba3a11f5f
fjktkm <fjktkm@gmail.com>
2024-05-09 15:43:55 +0000

    feat: Add Sec. 5.6

---
+++ src/sec5/subsec6/apply-operator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ghci <<EOF
+sum (filter (> 10) (map (*2) [2 .. 10]))
+sum $ filter (> 10) (map (*2) [2 .. 10])
+sum $ filter (> 10)  $ map (*2) [2 .. 10]
+map ($ 3) [(4 +), (10 *), (^ 2), sqrt]
+EOF

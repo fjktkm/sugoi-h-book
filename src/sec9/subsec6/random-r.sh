c516502674715601dfa19c40819409772500641d
fjktkm <fjktkm@gmail.com>
2024-05-31 03:59:41 +0000

    refactor: 型ヒントを追加

--- src/sec9/subsec6/random-r.sh
+++ src/sec9/subsec6/random-r.sh
@@ -4,5 +4,5 @@ ghci <<EOF
 :m +System.Random
 randomR (1, 6) (mkStdGen 359353)
 randomR (1, 6) (mkStdGen 35935335)
-take 10 $ randomRs ('a', 'z') (mkStdGen 35935335) 
+take 10 $ randomRs ('a', 'z') (mkStdGen 35935335) :: [Char]
 EOF

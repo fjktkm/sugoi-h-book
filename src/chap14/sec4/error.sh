fc51a0866194d3c0eee76ecbb5657fdbe82d221b
fjktkm <fjktkm@gmail.com>
2024-07-02 14:36:37 +0000

    feat: Add Chap.14 Sec.4

---
+++ src/chap14/sec4/error.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ghci <<EOF
+:m +Control.Monad.Error
+:t Right 4
+:t Left "out of cheese error"
+:t strMsg
+strMsg "boom!" :: String
+Left "boom" >>= \x -> return (x + 1)
+Left "boom " >>= \x -> Left "no way!"
+Right 100 >>= \x -> Left "no way!"
+Right 3 >>= \x -> return (x + 100)
+Right 3 >>= \x -> return (x + 100) :: Either String Int
+EOF
